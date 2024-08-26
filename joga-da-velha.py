72b6fab8239649f4b07f03d29b3c47f050e9ab7d
ovitortadeu <souavitor6@gmail.com>
2024-08-26 17:11:51 -0300

    pogtroll

--- joga-da-velha.py
+++ joga-da-velha.py
@@ -1,21 +1,44 @@
 
+
 def inicializarTabuleiro():
-    velha = [[0,0,0]
-            ,[0,0,0]
-            ,[0,0,0]]
+    velha = [['-','-','-'],
+             ['-','-','-'],
+             ['-','-','-']]
     return velha
-       
 
-def imprimirTabuleiro(inicializarTabuleiro):
-    print(f'''
-        {inicializarTabuleiro[0][0]}|{inicializarTabuleiro[0][1]}|{inicializarTabuleiro[0][2]}
-        {inicializarTabuleiro[1][0]}|{inicializarTabuleiro[1][1]}|{inicializarTabuleiro[1][2]}
-        {inicializarTabuleiro[2][0]}|{inicializarTabuleiro[2][1]}|{inicializarTabuleiro[2][2]}
-        ''')
-        
-    
+def imprimirTabuleiro(velha):
+    jogo_da_velha = (f'''
+            {velha[0][0]}|{velha[0][1]}|{velha[0][2]}
+            {velha[1][0]}|{velha[1][1]}|{velha[1][2]}
+            {velha[2][0]}|{velha[2][1]}|{velha[2][2]}
+                ''')
+    print(jogo_da_velha)
 
 
+def imprimeMenuPrincipal():
+    while True:
+        menu = int(input('''
+            Bem vindo ao Jogo da velha
+            ==========================
+            Qual modo você gostaria de jogar?
+            =================================
+            1 - JxJ
+            2 - JxM (fácil)
+            3 - JxM (difícil)
+            ==================================
+            Sua resposta: '''))
+        if menu > 3 or menu < 1:
+            print('Escolha uma opção de 1 a 3 por favor')
+        else:
+            return menu
+                
 
+
+
+
+
+
+
+imprimeMenuPrincipal()
 matriz = inicializarTabuleiro()
-imprimirTabuleiro(inicializarTabuleiro)
+imprimirTabuleiro(matriz)
